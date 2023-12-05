903f7cd9a17312e7e6d9a140c66fd70daf66042a
AtharvaNijasure <nijsureatharva@gmail.com>
2023-12-05 17:37:43 -0500

    extract conversation script

---
+++ Ecommerce/extract_conversations.py
@@ -0,0 +1,31 @@
+import json
+import threading
+
+
+lock = threading.Lock()
+
+
+def read_jsonl(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            json_obj = json.loads(line)
+            data.append(json_obj)
+    return data
+
+def write_jsonl(file_path, entry):
+    with open(file_path, 'a') as file:
+        json.dump(entry, file)
+        file.write('\n')
+
+
+
+filename = "./data/conversations6.jsonl"
+extracted_file = "./data/extracted_conversations.jsonl"
+data = read_jsonl(filename)
+
+for conversation in data :
+    del conversation['aux_cart']
+    del conversation['aux_compare']
+    
+    write_jsonl(extracted_file, conversation)
